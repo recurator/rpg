731de12daead1ea8be5da4edb51d5136120f36fc
Bilek Mirko <bilekmirko@gmail.com>
2019-11-27 14:33:00 +0100

    Change FactoryBot static attributes to dynamic

--- features/support/factories.rb
+++ features/support/factories.rb
@@ -2,8 +2,8 @@ require 'factory_bot'
 
 FactoryBot.define do
   factory :user do |f|
-    f.email 'jabre@example.net'
-    f.username 'jabre'
-    f.password 'secret'
+    f.email    { 'jabre@example.net' }
+    f.username { 'jabre' }
+    f.password { 'secret' }
   end
 end
